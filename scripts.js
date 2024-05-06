956003f19b2c3f50e887c2c83f50094517525e64
gideon-kirui <mail2kirui@gmail.com>
2024-05-06 07:21:13 +0300

    first commit-12

--- scripts.js
+++ scripts.js
@@ -34,60 +34,61 @@ var profLnks = document.querySelector('.profile-links');
 imgOpl.addEventListener('click', function(){
   profLnks.classList.toggle('active-pr');
 });
+document.addEventListener('DOMContentLoaded', function() {
+  const texts = [
+    {title: "Trusted & Secure Investment Company", text: "Are you ready for a great change!!! Online investment is now the talk of town, Invest on Dan's crypto today and experience a change that will shed smiles on you and your generations to come."},
+    {title: "Discover", text: "Praesent varius convallis risus, id eleifend dolor commodo id. Nulla malesuada mauris a nulla tempus, non convallis lectus interdum."},
+    {title: "Get Started", text: "Duis lacinia, sem a interdum laoreet, neque nunc convallis sapien, sit amet lacinia libero urna non sapien."},
+  ];
+  const images = ["./images/bn1.jpeg", "./images/bn6.jpeg", "./images/ban4.jpeg"]; // Add your image paths here
 
-const texts = [
-  {title: "Trusted & Secure Investment Company", text: "Are you ready for a great change!!! Online investment is now the talk of town, Invest on Dan's crypto today and experience a change that will shed smiles on you and your generations to come."},
-  {title: "Discover", text: "Praesent varius convallis risus, id eleifend dolor commodo id. Nulla malesuada mauris a nulla tempus, non convallis lectus interdum."},
-  {title: "Get Started", text: "Duis lacinia, sem a interdum laoreet, neque nunc convallis sapien, sit amet lacinia libero urna non sapien."},
-];
-const images = ["./images/bn1.jpeg", "./images/bn6.jpeg", "./images/ban4.jpeg"]; // Add your image paths here
+  let currentTitleIndex = 0;
+  let currentTextIndex = 0;
+  let currentImageIndex = 0;
 
-let currentTextIndex = 0;
-let currentImageIndex = 0;
+  // Function to change text and background image
+  function changeContent() {
+    document.getElementById("bannerTitle").textContent = texts[currentTitleIndex].title;
+    document.getElementById("bannerText").textContent = texts[currentTextIndex].text;
+    document.querySelector(".banner").style.backgroundImage = `url(${images[currentImageIndex]})`;
+  }
 
-// Function to change text and background image
-function changeContent() {
-  document.getElementById("bannerTitle").textContent = texts[currentTextIndex].title;
-  document.getElementById("bannerText").textContent = texts[currentTextIndex].text;
-  document.querySelector(".banner").style.backgroundImage = `url(${images[currentImageIndex]})`;
-}
+  // Call changeContent function once when the page loads
+  changeContent();
 
-// Call changeContent function once when the page loads
-changeContent();
+  document.getElementById("bannerTitle").classList.add("slide-up");
+  document.getElementById("bannerText").classList.add("slide-right");
 
-document.getElementById("bannerTitle").classList.add("slide-up");
-document.getElementById("bannerText").classList.add("slide-right");
+  // Function to change text and background image on interval
+  function changeContentOnInterval() {
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.getElementById("bannerTitle").classList.remove("slide-up");
+    document.getElementById("bannerText").classList.remove("slide-right");
+    setTimeout(() => {
+      changeContent();
+      document.getElementById("bannerTitle").classList.add("slide-up");
+      document.getElementById("bannerText").classList.add("slide-right");
+    }, 100);
+  }
 
-// Function to change text and background image on interval
-function changeContentOnInterval() {
-  currentTextIndex = (currentTextIndex + 1) % texts.length;
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  document.getElementById("bannerTitle").classList.remove("slide-up");
-  document.getElementById("bannerText").classList.remove("slide-right");
-  setTimeout(() => {
+  // Start interval for automatic animation
+  setInterval(changeContentOnInterval, 4000);
+
+  // Function to handle next button click
+  document.getElementById("nextBtn").addEventListener("click", function() {
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
+    currentImageIndex = (currentImageIndex + 1) % images.length;
     changeContent();
-    document.getElementById("bannerTitle").classList.add("slide-up");
-    document.getElementById("bannerText").classList.add("slide-right");
-  }, 100);
-}
+  });
 
-// Start interval for automatic animation
-setInterval(changeContentOnInterval, 4000);
-
-// Function to handle next button click
-document.getElementById("nextBtn").addEventListener("click", function() {
-  currentTextIndex = (currentTextIndex + 1) % texts.length;
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  changeContent();
+  // Function to handle previous button click
+  document.getElementById("prevBtn").addEventListener("click", function() {
+    currentTextIndex = (currentTextIndex - 1 + texts.length) % texts.length;
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    changeContent();
+  });
 });
-
-// Function to handle previous button click
-document.getElementById("prevBtn").addEventListener("click", function() {
-  currentTextIndex = (currentTextIndex - 1 + texts.length) % texts.length;
-  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-  changeContent();
-});
-
 // Users Statistics
 window.addEventListener('scroll', () => {
   const numbers = document.querySelectorAll('.card-title');
@@ -106,29 +107,41 @@ window.addEventListener('scroll', () => {
 });
 
 // JavaScript code for auto-sliding testimonials
-const testimonials = document.querySelectorAll('.testimonial');
-let index = 0;
+document.addEventListener('DOMContentLoaded', function() {
+  const testimonials = document.querySelectorAll('.testimonial');
+  let index = 0;
 
-function showTestimonial() {
-  testimonials.forEach(testimonial => {
-    testimonial.classList.remove('active');
-  });
-  testimonials[index].classList.add('active');
-  index = (index + 1) % testimonials.length;
-}
+  function showTestimonial() {
+    testimonials.forEach(testimonial => {
+      testimonial.classList.remove('active');
+    });
+    testimonials[index].classList.add('active');
+    index = (index + 1) % testimonials.length;
+  }
 
-setInterval(showTestimonial, 5000);
-
+  setInterval(showTestimonial, 5000);
+});
 
 // Displaying depo pop
-var clsBtn = document.querySelector('.fa-circle-xmark');
-var opnBtn = document.querySelector('.pn-dfrm');
-var dsplyForm = document.querySelector('.inv-fm');
+var openDepo = document.querySelector('.opn-depo');
+var closeDepo = document.querySelector('.fa-circle-xmark');
+var pnDepp = document.getElementById('inv-fm');
 
-opnBtn.addEventListener('click', function(){
-  dsplyForm.classList.add('active');
+openDepo.addEventListener('click', function(){
+  pnDepp.classList.add('active-p');
+});
+closeDepo.addEventListener('click', function(){
+  pnDepp.classList.remove('active-p');
 });
 
-clsBtn.addEventListener('click', function(){
-  dsplyForm.classList.remove('active');
-});
+// // Displaying depo pop
+// var openWdrwl = document.querySelector('.opn-wdrwl');
+// var closeWdrwl = document.querySelector('.fa-circle-xmark');
+// var pnWdrwl = document.getElementById('wdthrw-fm');
+
+// openWdrwl.addEventListener('click', function(){
+//   pnWdrwl.classList.add('active-w');
+// });
+// closeWdrwl.addEventListener('click', function(){
+//   pnWdrwl.classList.remove('active-w');
+// });
